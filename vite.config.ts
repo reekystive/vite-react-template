871a7e49f31a4c9f661d065923f0aef06ca41702
ReekyStive <reekystive@gmail.com>
2023-09-14 00:26:48 +0800

    vite: ✨ add vite-plugin-sass-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import sassDts from 'vite-plugin-sass-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sassDts()],
 });
