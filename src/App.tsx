62447de5fda14f47ba16f3bddc17f8e881c31b9f
Mingxuan Wang <reekystive@gmail.com>
2023-12-07 21:27:58 +0800

    feat: ✨ simple swr posts demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,94 @@
-import { useState } from 'react';
+import { FC } from 'react';
+import { Link, Route, Routes, useParams } from 'react-router-dom';
+import useSWR from 'swr';
 import './App.scss';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
+const LOREM_IPSUM = `Anim id laboris ipsum ex adipisicing eiusmod. Cillum aute ex consectetur velit ea eiusmod culpa ad aliqua eiusmod cupidatat sint. Et anim cupidatat sunt tempor. Est commodo ut in ad Lorem est. Laboris tempor commodo est ullamco. Quis culpa id Lorem ea sint mollit labore cillum ut ullamco ullamco consequat. Cupidatat sunt minim quis exercitation commodo cillum. Irure laboris mollit consectetur in.`;
+const POSTS = [
+  { id: '1', title: 'Reprehenderit adipisicing cillum cillum.', content: LOREM_IPSUM },
+  { id: '2', title: 'Cillum nostrud exercitation anim laborum nulla.', content: LOREM_IPSUM },
+  { id: '3', title: 'Est Lorem esse enim ex mollit dolor officia.', content: LOREM_IPSUM },
+  { id: '4', title: 'Ex proident laborum aliqua voluptate non magna cupidatat.', content: LOREM_IPSUM },
+];
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function getPost(postId: string) {
+  await delay(1000);
+  return POSTS.find((post) => post.id === postId);
+}
+
+async function getPostList() {
+  await delay(1000);
+  return POSTS.map((post) => ({
+    id: post.id,
+    title: post.title,
+    summary: post.content.slice(0, 123) + '...',
+  }));
+}
+
+function usePost({ postId }: { postId: string }) {
+  const { data, isLoading, isValidating } = useSWR(['post', postId], () => getPost(postId));
+  return { data, isLoading, isValidating };
+}
+
+function usePostList() {
+  const { data, isLoading, isValidating } = useSWR('posts', getPostList);
+  return { data, isLoading, isValidating };
+}
+
+const Post: FC = () => {
+  const { postId } = useParams();
+  if (!postId) {
+    return <div>Post Not Found</div>;
+  }
+  const { data, isLoading, isValidating } = usePost({ postId });
+  return (
+    <div>
+      <Link to="/" className="block py-2 text-2xl">
+        ⬅️ Back
+      </Link>
+      <h2 className="py-2 text-2xl font-bold">{data?.title}</h2>
+      <p>{data?.content}</p>
+      <div className="py-2 text-red-500">
+        {isLoading && <div>Loading...</div>}
+        {isValidating && <div>Validating...</div>}
+      </div>
+    </div>
+  );
+};
+
+const PostList: FC = () => {
+  const { data, isLoading, isValidating } = usePostList();
+  return (
+    <div>
+      <h1 className="py-2 text-2xl font-bold">Posts</h1>
+      <ul>
+        {data?.map((post) => (
+          <li key={post.id}>
+            <Link to={`/posts/${post.id}`} className="block py-2 text-2xl">
+              {post.title}
+            </Link>
+            <p>{post.summary}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="py-2 text-red-500">
+        {isLoading && <div>Loading...</div>}
+        {isValidating && <div>Validating...</div>}
+      </div>
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo inline" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react inline" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="my-[0.5em] font-bold">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p className="my-[1em]">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Routes>
+      <Route path="/" element={<PostList></PostList>} />
+      <Route path="/posts/:postId" element={<Post></Post>} />
+    </Routes>
   );
 }
 
