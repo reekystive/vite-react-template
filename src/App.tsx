cc2038d52ddc9064ea9123fdb52539641f97e2de
Mingxuan Wang <reekystive@gmail.com>
2023-12-08 13:24:11 +0800

    feat: ✨ swr posts demo without swr

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Link, Route, Routes, useParams } from 'react-router-dom';
-import useSWR from 'swr';
 import './App.scss';
 
+type Post = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type PostList = {
+  id: string;
+  title: string;
+  summary: string;
+}[];
+
 const LOREM_IPSUM = `Anim id laboris ipsum ex adipisicing eiusmod. Cillum aute ex consectetur velit ea eiusmod culpa ad aliqua eiusmod cupidatat sint. Et anim cupidatat sunt tempor. Est commodo ut in ad Lorem est. Laboris tempor commodo est ullamco. Quis culpa id Lorem ea sint mollit labore cillum ut ullamco ullamco consequat. Cupidatat sunt minim quis exercitation commodo cillum. Irure laboris mollit consectetur in.`;
-const POSTS = [
+const POSTS: Post[] = [
   { id: '1', title: 'Reprehenderit adipisicing cillum cillum.', content: LOREM_IPSUM },
   { id: '2', title: 'Cillum nostrud exercitation anim laborum nulla.', content: LOREM_IPSUM },
   { id: '3', title: 'Est Lorem esse enim ex mollit dolor officia.', content: LOREM_IPSUM },
@@ -15,12 +26,12 @@ function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function getPost(postId: string) {
+async function getPost(postId: string): Promise<Post | undefined> {
   await delay(1000);
   return POSTS.find((post) => post.id === postId);
 }
 
-async function getPostList() {
+async function getPostList(): Promise<PostList> {
   await delay(1000);
   return POSTS.map((post) => ({
     id: post.id,
@@ -30,13 +41,27 @@ async function getPostList() {
 }
 
 function usePost({ postId }: { postId: string }) {
-  const { data, isLoading, isValidating } = useSWR(['post', postId], () => getPost(postId));
-  return { data, isLoading, isValidating };
+  const [data, setData] = useState<Post | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getPost(postId).then((post) => {
+      setData(post);
+      setIsLoading(false);
+    });
+  }, []);
+  return { data, isLoading };
 }
 
 function usePostList() {
-  const { data, isLoading, isValidating } = useSWR('posts', getPostList);
-  return { data, isLoading, isValidating };
+  const [data, setData] = useState<PostList | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    getPostList().then((postList) => {
+      setData(postList);
+      setIsLoading(false);
+    });
+  }, []);
+  return { data, isLoading };
 }
 
 const Post: FC = () => {
@@ -44,7 +69,7 @@ const Post: FC = () => {
   if (!postId) {
     return <div>Post Not Found</div>;
   }
-  const { data, isLoading, isValidating } = usePost({ postId });
+  const { data, isLoading } = usePost({ postId });
   return (
     <div>
       <Link to="/" className="block py-2 text-2xl">
@@ -52,16 +77,13 @@ const Post: FC = () => {
       </Link>
       <h2 className="py-2 text-2xl font-bold">{data?.title}</h2>
       <p>{data?.content}</p>
-      <div className="py-2 text-red-500">
-        {isLoading && <div>Loading...</div>}
-        {isValidating && <div>Validating...</div>}
-      </div>
+      <div className="py-2 text-red-500">{isLoading && <div>Loading...</div>}</div>
     </div>
   );
 };
 
 const PostList: FC = () => {
-  const { data, isLoading, isValidating } = usePostList();
+  const { data, isLoading } = usePostList();
   return (
     <div>
       <h1 className="py-2 text-2xl font-bold">Posts</h1>
@@ -75,10 +97,7 @@ const PostList: FC = () => {
           </li>
         ))}
       </ul>
-      <div className="py-2 text-red-500">
-        {isLoading && <div>Loading...</div>}
-        {isValidating && <div>Validating...</div>}
-      </div>
+      <div className="py-2 text-red-500">{isLoading && <div>Loading...</div>}</div>
     </div>
   );
 };
