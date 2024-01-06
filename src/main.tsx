ad7140d4e2ddb59649651cb998d891b5e4be8765
Mingxuan Wang <reekystive@gmail.com>
2024-01-07 00:46:12 +0800

    fix: 🐛 eslint error

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { App } from './app.tsx';
 import './global.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root');
+if (!root) throw new Error('No root element');
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
