0d788bdd8e9e83103d7725040f9e74deb4d61b16
Mingxuan Wang <wangmingxuan.io@bytedance.com>
2025-06-05 02:11:44 +0800

    chore: 🔧 update react version in eslint.config.mjs to 19.1.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -65,7 +65,7 @@ const eslintConfig = [
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
     settings: {
-      react: { version: '18.3.1' },
+      react: { version: '19.1.0' },
     },
   },
   {
